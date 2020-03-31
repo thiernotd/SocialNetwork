f73807449814fc8747238256472e4c007f525fbf
Adianna <56278008+Adianna@users.noreply.github.com>
2020-03-31 20:06:39 +0000

    Add files via upload

---
+++ src/Serveur/Comment.java
@@ -0,0 +1,70 @@
+package Serveur;
+
+import java.util.Date;
+
+public class Comment{
+	public Date date;
+	public int idCommentaire;
+	public int idUser;
+	public String comment;
+	public String user;
+	public int pidCommentaire;
+	public int pidMessage;
+
+	public Comment(Date date,int idCmnt,int idUsr,String cmnt,String usr)
+	{
+		this.date=date;
+		this.idCommentaire = idCmnt;
+		this.idUser= idUsr;
+		this.comment = cmnt;
+		this.user = usr;	
+	}
+	public Date getDate(){
+		return this.date;
+	}
+	public int getIdComment(){
+		return this.idCommentaire;
+	}
+	public int getIdUser(){
+		return this.idUser;
+	}
+	public String getComment(){
+		return this.comment;
+	}
+	public String getUser(){
+		return this.user;
+	}
+	public int getPidCommentaire(){
+		return this.pidCommentaire;
+	}
+	public int getPidMessage(){
+		return this.pidMessage;
+	}
+	public void setDate(Date date){
+		this.date = date;
+	}
+	public void setIdComment(int idCmnt){
+		this.idCommentaire=idCmnt;
+	}
+	public void setIdUser(int idUsr){
+		this.idUser = idUsr;
+	}
+	public void setComment(String cmnt){
+		this.comment = cmnt;
+	}
+	public void setUser(String usr){
+		this.user=usr;
+	}
+	public void setPidCommentaire(int pidCmnt){
+		this.pidCommentaire = pidCmnt;
+	}
+	public void setPidMessage(int pidMsg){
+		this.pidMessage = pidMsg;
+	}
+	public String toString(){
+		String c = "";
+		c+=this.date+" "+this.idCommentaire+" "+this.idUser+" "+this.comment+" "+this.user;
+		return c;
+	}
+
+}
